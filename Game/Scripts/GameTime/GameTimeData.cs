9320b5ad31ef0006f7e9a22eca117c7287a597e3
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-07-16 15:16:58 -0500

    qe

--- Game/Scripts/GameTime/GameTimeData.cs
+++ Game/Scripts/GameTime/GameTimeData.cs
@@ -1,13 +1,44 @@
 using UnityEngine;
 
-[System.Serializable]
+
 [CreateAssetMenu (menuName = "Time/GameTimeData")]
 public class GameTimeData : ScriptableObject
 {
 
+
     public Day Day;
     public float Time;
+
+    public int Year; // MAYBE
+
+    public Action<Day> OnDayChanged;
+    public Action<Month> OnMonthChanged;
+    public Action<int> OnYearChanged;
+
+    public void ChangeDay(Day newDay) 
+    {
+        OnDayChanged?.Invoke(newDay);
+
+    }
+
+    public void ChangeMonth(Month newMonth)
+    {
+        OnMonthChanged?.Invoke(newMonth);
+
+    }
+    /// <summary>
+    /// <br> Adds +1 to the Year count. <br> 
+    /// <br> Not implemented yet. MAYBE. </br>
+    /// </summary>
+    public void ChangeYear() 
+    {
+        Year++;
+        OnYearChanged?.Invoke(Year);
+
+    }
+
 }
+
 
 public enum Day 
 {
@@ -18,5 +49,24 @@ public enum Day
     Friday, 
     Saturday, 
     Sunday,
+}
+
+
+public enum Month
+{
+    January,
+    Febuary,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+
+
 }
 
