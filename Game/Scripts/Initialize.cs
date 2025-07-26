8dfb939076d51565552daf8bc0849edbb05bfe2e
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-07-25 23:07:16 -0500

    qwedasd

--- Game/Scripts/Initialize.cs
+++ Game/Scripts/Initialize.cs
@@ -1,14 +1,26 @@
 using MyBox;
 using UnityEngine;
 
-
+public enum InitializeType
+{
+    None,
+    TriggerSceneLoad,
+    TriggerOnSceneLoad,
+}
 
 public class Initialize : MonoBehaviour
 {
     [SerializeField] private bool _active;
 
-    [SerializeField] private bool _triggerSceneLoad;
-    [ConditionalField(nameof(_triggerSceneLoad))][SerializeField] private SceneLoadTrigger _sceneTrigger;
+    [ConditionalField(nameof(_active))]
+    [SerializeField] private InitializeType _initializeType = InitializeType.None;
+
+
+    [ConditionalField(nameof(_initializeType), false, InitializeType.TriggerSceneLoad)]
+    [SerializeField] private SceneLoadTrigger _sceneTrigger;
+
+    [ConditionalField(nameof(_initializeType), false, InitializeType.TriggerOnSceneLoad)]
+    [SerializeField] private bool test;
 
     [Header("Initilize Pool")]
     [DisplayInspector]
@@ -23,7 +35,7 @@ public class Initialize : MonoBehaviour
         {
             _pool.CreatePool();
             _pool.InstantiatePoolObjects();
-            if (_triggerSceneLoad)
+            if (_initializeType is InitializeType.TriggerSceneLoad)
             {
                 _sceneTrigger.Trigger(SceneLoadingSettings.MainMenu); // trigger scene load after it's done loading the pool objects
             }
